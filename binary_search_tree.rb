0874177cb3fcc3aaaa5cbd9ab3d67502543d5f2c
Logravia <j.alunans@gmail.com>
2022-02-02 16:14:07 +0200

    Add Comparable module to Node

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,4 +1,14 @@
 #!/usr/bin/env ruby
 
 class Node
+  include Comparable
+  attr_accessor :data, :left, :right
+  def initialize(data = nil)
+    @data = data
+    @left = nil
+    @right = nil
+  end
+  def <=>(other_node)
+    @data <=> other_node.data
+  end
 end
