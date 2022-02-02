bc96d5d6131b92424da5d0fe7ac4b79f5bfe6bf4
Logravia <j.alunans@gmail.com>
2022-02-03 00:15:49 +0200

    Rubocop guided refactor and spacing changes

--- binary_tree.rb
+++ binary_tree.rb
@@ -1,24 +1,35 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'pry-byebug'
 require_relative 'node'
 
 class Tree
-  def initialize(arr=[])
+  def initialize(arr = [])
     @root = build_tree(arr)
   end
+
   def build_tree(arr)
     arr.sort!.uniq!
     build_nodes(arr)
   end
+
   def build_nodes(arr)
     return Node.new(arr.first) if arr.size < 2
-    mid_point = arr.size/2
+
+    mid_point = arr.size / 2
     mid_node = Node.new(arr[mid_point])
     mid_node.left = build_nodes(arr[0...mid_point])
-    mid_node.right = build_nodes(arr[mid_point+1..arr.size])
+    mid_node.right = build_nodes(arr[mid_point + 1...arr.size])
+
     mid_node
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
 
-pp Tree.new([1,2,3,4,5,6,7])
+Tree.new(arr).pretty_print
--- node.rb
+++ node.rb
@@ -1,14 +1,17 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 class Node
   include Comparable
   attr_accessor :data, :left, :right
+
   def initialize(data = nil)
     @data = data
     @left = nil
     @right = nil
   end
-  def <=>(other_node)
-    @data <=> other_node.data
+
+  def <=>(other)
+    @data <=> other.data
   end
 end
